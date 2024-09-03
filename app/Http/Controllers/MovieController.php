6f95ac2753dc897769f22b1ad401b972749192b0
mmccjjj <dhuber88@sunrise.ch>
2024-09-03 16:59:06 +0200

    validate

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -31,6 +31,16 @@ class MovieController extends Controller
 
     function store(Request $request)
     {
-        return dd($request);
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'required|string|max:350',
+            'released_at' => 'required|date',
+            'length_in_minutes' => 'required|integer|min:1',
+            'budget' => 'required|numeric|min:0',
+            'based_on_a_book' => 'boolean',
+            'score' => 'required|numeric|min:0|max:10',
+            'original_language' => 'required|string|max:255'
+        ]);
+        return dd($request->all());
     }
 }
--- resources/views/movie/create.blade.php
+++ resources/views/movie/create.blade.php
@@ -11,25 +11,52 @@
     <form action="{{ route('movies.store') }}" method="POST">
         @csrf
         <label for="name">Name</label>
-        <input type="text" name="name" id="name">
+        <input type="text" name="name" id="name" value='{{ old("name") }}'>
+        @error('name')
+        <div class="input-error">{{$message}}</div>
+        @enderror
 
         <label for="description">Description</label>
-        <textarea name="description" id="description" placeholder="Description..."></textarea>
+        <textarea name="description" id="description" placeholder="Description..." value='{{ old("description") }}'></textarea>
+        @error('description')
+        <div class="input-error">{{$message}}</div>
+        @enderror
 
         <label for="released_at">Released_at</label>
-        <input type="date" name="released_at" id="released_at">
+        <input type="date" name="released_at" id="released_at" value='{{ old("released_at") }}'>
+        @error('released_at')
+        <div class="input-error">{{$message}}</div>
+        @enderror
 
         <label for="length_in_minutes">Length in minutes</label>
-        <input type="number" name="length_in_minutes" id="length_in_minutes">
+        <input type="number" name="length_in_minutes" id="length_in_minutes" value='{{ old("length_in_minutes") }}'>
+        @error('length_in_minutes')
+        <div class="input-error">{{$message}}</div>
+        @enderror
+
+        <label for="budget">Budget</label>
+        <input type="number" name="budget" id="budget" value='{{ old("budget") }}'>
+        @error('budget')
+        <div class="input-error">{{$message}}</div>
+        @enderror
 
         <label for="based_on_a_book">Based on a book</label>
-        <input type="checkbox" name="based_on_a_book" id="based_on_a_book">
+        <input type="checkbox" name="based_on_a_book" id="based_on_a_book" value="{{ old('based_on_a_book', 0) ? 'checked' : '' }}">
+        @error('based_on_a_book')
+        <div class="input-error">{{$message}}</div>
+        @enderror
 
         <label for="score">Score</label>
-        <input type="number" name="score" id="score">
+        <input type="number" name="score" id="score" value='{{ old("score") }}'>
+        @error('score')
+        <div class="input-error">{{$message}}</div>
+        @enderror
 
         <label for="original_language">Original language</label>
-        <input type="text" name="original_language" id="original_language">
+        <input type="text" name="original_language" id="original_language" value='{{ old("original_language") }}'>
+        @error('original_language')
+        <div class="input-error">{{$message}}</div>
+        @enderror
 
         <button type="submit">Submit</button>
 
